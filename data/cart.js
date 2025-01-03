3fd52798532a327da5205149edda657f18035cbc
Gintare Ragaisiene <gintare.ragaisiene@gmail.com>
2025-01-03 18:54:28 +0200

    Create automated test for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,22 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if(!cart) {
-   cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-    deliveryOptionId: '1'
-  }, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-    deliveryOptionId: '2'
-  }];
+
+loadFromStorage();
+
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart'));
+
+  if (!cart) {
+    cart = [{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
+    }, {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '2'
+    }];
+  }
 }
 
 function saveToStorage() {
@@ -18,32 +25,32 @@ function saveToStorage() {
 
 export function addToCart(productId) {
 
-  let matchingItem
-      cart.forEach((cartItem) => {
-         if(cartItem.productId === productId){
-          matchingItem = cartItem;
-         }
-      });
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if (cartItem.productId === productId) {
+      matchingItem = cartItem;
+    }
+  });
 
-      if(matchingItem){
-         matchingItem.quantity += 1;
-      } else {
-        cart.push({
-          productId: productId,
-          quantity: 1,
-          deliveryOptionId: '1'
-         });
-        }
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+      deliveryOptionId: '1'
+    });
+  }
   saveToStorage();
 }
 
 export function removeFromCart(productId) {
   const newCart = [];
-  
+
   cart.forEach((cartItem) => {
-      if(cartItem.productId !== productId){
-         newCart.push(cartItem);
-      }
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
   });
 
   cart = newCart;
@@ -54,9 +61,9 @@ export function removeFromCart(productId) {
 export function updateDeliveryOption(productId, deliveryOptionId) {
   let matchingItem
   cart.forEach((cartItem) => {
-     if(cartItem.productId === productId){
+    if (cartItem.productId === productId) {
       matchingItem = cartItem;
-     }
+    }
   });
 
   matchingItem.deliveryOptionId = deliveryOptionId;
