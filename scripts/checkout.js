b8f995d1e93da582f2d86e000abe4f200892e5e6
Gintare Ragaisiene <gintare.ragaisiene@gmail.com>
2025-01-09 18:34:12 +0200

    create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummury } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import "../data/cart-oop.js";
+import "../data/cart-class.js";
 
 renderOrderSummury();
 renderPaymentSummary();
