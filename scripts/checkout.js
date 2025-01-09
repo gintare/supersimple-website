a05c12c2da49c4cde5c519e830a4cfcceaba8a73
Gintare Ragaisiene <gintare.ragaisiene@gmail.com>
2025-01-09 17:54:15 +0200

    Create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrderSummury } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import "../data/cart-oop.js";
 
 renderOrderSummury();
 renderPaymentSummary();
