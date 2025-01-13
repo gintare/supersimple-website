ab89490654c5e61cea0578f151bb98f72cf15c91
Gintare Ragaisiene <gintare.ragaisiene@gmail.com>
2025-01-13 18:30:04 +0200

    Load products from backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,10 @@
 import { renderOrderSummury } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 //import "../data/cart-class.js";
-import "../data/backend-practice.js";
+//import "../data/backend-practice.js";
+import { loadProducts } from "../data/products.js";
 
-renderOrderSummury();
-renderPaymentSummary();
+loadProducts(() => {
+  renderOrderSummury();
+  renderPaymentSummary();
+});
